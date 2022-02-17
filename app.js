eb522dc1baaf1c09b5ca50db0b6bf707f87ab80e
Carlos Vargas <varlopecar@gmail.com>
2022-02-16 22:51:53 -0600

    Inputs

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
-let container = document.getElementById("container");
+const btnSend = document.getElementById("btnSend");
+
+btnSend.addEventListener("click", () => {
+  const txtRows = Number.parseInt(document.getElementById("txtRows").value);
+  const txtCols = Number.parseInt(document.getElementById("txtCols").value);
+  console.log(txtRows);
+  console.log(txtCols);
+  makeRows(txtRows, txtCols);
+});
 
 function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows); //set the property --grid-rows to container
-  container.style.setProperty('--grid-cols', cols); //set the property --grid-cols to container
-  for (c = 0; c < (rows * cols); c++) {
+  container.style.setProperty("--grid-rows", rows); //set the property --grid-rows to container
+  container.style.setProperty("--grid-cols", cols); //set the property --grid-cols to container
+  for (c = 0; c < rows * cols; c++) {
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
-  };
-  hoverColor()
-};
+  }
+  hoverColor();
+}
 
-const randomColor = () => {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for(let i = 0; i < 6; i++){
+function randomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
 }
 
-console.log(randomColor());
-
 function hoverColor() {
-  let items = document.querySelectorAll('.grid-item');
-  items.forEach(item => {
-    item.addEventListener('mouseover', () => {
+  let items = document.querySelectorAll(".grid-item");
+  items.forEach((item) => {
+    item.addEventListener("mouseover", () => {
       item.style.backgroundColor = `${randomColor()}`;
     });
   });
 }
-
-makeRows(16, 16);
